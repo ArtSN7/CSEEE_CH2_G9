5d102fa7ea57c96fa5799a53fdf07cec0422e9f6
Shay O'Connor <shaywoconnor@hotmail.com>
2025-11-12 10:43:25 +0000

    sensor class

--- src/Controller/sensor.h
+++ src/Controller/sensor.h
@@ -1,13 +1,26 @@
 #ifndef SENSOR_H
 #define SENSOR_H
 
+struct sensor_reading
+{
+  double value;
+  uint32_t timestamp;
+  bool valid;
+};
+
 class Sensor
 {
   public:
+    Sensor()
+    SPI_Receiver* getReceiver();
+    struct sensor_reading getReading();
   protected:
+    SPI_Receiver receiver;
+    struct sensor_reading reading;
   private:
 }
 
+//these might be unneeded, but better for clarity, most is abstracted already in Sensor class
 class PH_Sensor : public Sensor
 {
   public:
